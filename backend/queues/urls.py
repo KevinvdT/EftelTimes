4056d7538e37ef2b0b7905c914f1961a7140e5f8
kvandertoorn <kevinvdtoorn@live.nl>
2026-05-22 17:30:20 +0200

    fix: update attraction queue times endpoint to version 2 in URLs

--- backend/queues/urls.py
+++ backend/queues/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('api/v1/queues/', views.queue_times, name='queue_times'),
-    path('api/v1/queues/attractions/', views.attraction_queue_times, name='attraction_queue_times'),
+    path('api/v2/queues/attractions/', views.attraction_queue_times, name='attraction_queue_times'),
     path('api/v1/opening-hours/', views.opening_hours, name='opening_hours'),
 ] 
